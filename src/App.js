700ae169718f36f9a637c8593130b8e8f44ef72a
Greg <gregt87@gmail.com>
2017-12-12 02:26:25 -0500

    tried to add survey monkey

--- src/App.js
+++ src/App.js
@@ -2,6 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const surveyMonkeyScript = '(function(t,e,s,o){var n,c,l;t.SMCX=t.SMCX||[],e.getElementById(o)||(n=e.getElementsByTagName(s),c=n[n.length-1],l=e.createElement(s),l.type="text/javascript",l.async=!0,l.id=o,l.src=["https:"===location.protocol?"https://":"http://","widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgdzkkVYO_2BCXQEYGXfMQEai55HQEK4lQe8CkzjknXTMIb2.js"].join(""),c.parentNode.insertBefore(l,c))})(window,document,"script","smcx-sdk");';
+
+class SurveyScript extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const s = document.createElement('script');
+    // s.type = 'text/javascript';
+    // s.async = true;
+    s.innerHTML = surveyMonkeyScript;
+    this.instance.appendChild(s);
+  }
+  render() {
+    return <div ref={el => (this.instance = el)} />;
+  }
+}
+
+class SurveyButton extends Component {
+  constructor() {
+    super();
+    this.state = { showSurvey: false };
+  }
+  render() {
+    return (
+      <div>
+        <button type="button" className="btn btn-primary" onClick={() => this.setState({showSurvey: true})}>Join us</button>
+        {this.state.showSurvey && <SurveyScript />}
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -37,51 +71,33 @@ class App extends Component {
             </div>
             <div className="col-4">
               Empowerment
-              <i class="fa xfa-graduation-cap" aria-hidden="true"></i>
+              <i className="fa xfa-graduation-cap" aria-hidden="true"></i>
             </div>
           </div>
           <div className="row App-section">
             <div className="col-4">
-              <i class="App-icon fa fa-universal-access" aria-hidden="true"></i>
+              <i className="App-icon fa fa-universal-access" aria-hidden="true"></i>
             </div>
             <div className="col-4">
-              <i class="App-icon fa fa-graduation-cap" aria-hidden="true"></i>
+              <i className="App-icon fa fa-graduation-cap" aria-hidden="true"></i>
             </div>
             <div className="col-4">
-              <i class="App-icon fa fa-hand-rock-o" aria-hidden="true"></i>
+              <i className="App-icon fa fa-hand-rock-o" aria-hidden="true"></i>
             </div>
           </div>
           <div className="row App-section">
-            <div className="col-4">
+            <div className="col-12">
               <p className="App-missionText">
                 Ever feel lost, adrift not really sure where life is taking you.  
                 This is a unifying feeling that a lot of people experience.  
-                Most places around the world, have lost the intimate connection of the master/apprentice system in lou of more traditional school environments.  
-                We value close connections that will take encorage people to joining tight communities.  
+                Most places around the world, have lost the intimate connection of the master/apprentice system in leiu of traditional school environments.  
+                We value close connections that will encourage people to joining tight communities.  
                 Our mission is to support people by finding like-minded individuals and mentors and empowering them to mentor as well.
               </p>
             </div>
-            <div className="col-8">
-              <div className="App-nameEmail">
-                <div>
-                  Name
-                  <textarea className="App-textarea App-shortTextarea"></textarea>
-                </div>
-                <div>
-                  Email
-                  <textarea className="App-textarea App-shortTextarea"></textarea>
-                </div>
-              </div>
-              <div>
-                Subject
-                <textarea className="App-textarea"></textarea>
-              </div>
-              <div>
-                Message
-                <textarea className="App-textarea App-messageTextarea"></textarea>
-              </div>
-              <div className="App-submitMessageButtonArea">
-                <button type="button" class="btn btn-primary">Submit Message</button>
+            <div className="col-12">
+              <div className="App-surveyButton">
+                <SurveyButton/>
               </div>
             </div>
           </div>
